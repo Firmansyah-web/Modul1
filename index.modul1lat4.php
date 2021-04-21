41c95eaa34b7bc7f444e8cec0887a358684b6328
Firmansyah-web <mansyahfir17@gmail.com>
2021-04-21 11:22:45 +0700

    modul1

---
+++ index.modul1lat4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Belajar php</title>
+</head>
+<body>
+<?php 
+Define("judul", "Menghitung luas lingkaran"); 
+define ("phi",3.14) ; 
+$r = 5; 
+$luas = phi * $r * $r ; 
+echo judul;
+echo " dengan jari-jari $r = $luas"; 
+?>
+</body>
+</html>
